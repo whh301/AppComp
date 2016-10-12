b2278b16652b2efbce317a77cdffbe996429428f
icewitch <bing.yuan@spirent.com>
2016-10-12 16:39:03 +0800

    delete duplicate codes in init method

--- app/src/main/java/com/spirent/landslide/datagen/MainActivity.java
+++ app/src/main/java/com/spirent/landslide/datagen/MainActivity.java
@@ -60,65 +60,6 @@ public class MainActivity extends AppCompatActivity {
         mLabelCellId = (TextView)findViewById(R.id.lblCellId);
         // Get Cell Id and etc
         telephonyManager =(TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-        int netWorkType = telephonyManager.getPhoneType();
-        String cellId = "";
-        try {
-            if (netWorkType == TelephonyManager.PHONE_TYPE_CDMA) {
-                CdmaCellLocation location = (CdmaCellLocation) telephonyManager.getCellLocation();
-                cellId = "" + location.getBaseStationId();
-            } else {
-                GsmCellLocation location = (GsmCellLocation) telephonyManager.getCellLocation();
-                cellId = "" + location.getCid();
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        mLabelCellId.setText(cellId);
-
-        int ntwkType=telephonyManager.getNetworkType();
-        String ntwkTypeStr="ERR";
-        switch (ntwkType) {
-            case 0: ntwkTypeStr = "UNKNOWN";
-                break;
-            case 1: ntwkTypeStr = "GPRS";
-                break;
-            case 2: ntwkTypeStr = "EDGE";
-                break;
-            case 3: ntwkTypeStr = "UMTS";
-                break;
-            case 4: ntwkTypeStr = "CDMA";
-                break;
-            case 5: ntwkTypeStr = "EVDO_0";
-                break;
-            case 6: ntwkTypeStr = "EVDO_A";
-                break;
-            case 7: ntwkTypeStr = "1xRTT";
-                break;
-            case 8: ntwkTypeStr = "HSDPA";
-                break;
-            case 9: ntwkTypeStr = "HSUPA";
-                break;
-            case 10: ntwkTypeStr = "HSPA";
-                break;
-            case 11: ntwkTypeStr = "IDEN";
-                break;
-            case 12: ntwkTypeStr = "EVDO_B";
-                break;
-            case 13: ntwkTypeStr = "LTE";
-                break;
-            case 14: ntwkTypeStr = "EHRPD";
-                break;
-            case 15: ntwkTypeStr = "HSPAP";
-                break;
-            case 16: ntwkTypeStr = "GSM";
-                break;
-            case 17: ntwkTypeStr = "TD_SCDMA";
-                break;
-            case 18: ntwkTypeStr = "IWLAN";
-                break;
-        }
-
-        mLabelNetwork.setText(ntwkTypeStr);
 
         mLabelCarrier.setText(telephonyManager.getNetworkOperatorName());
         mLabelLocation.setText(telephonyManager.getNetworkCountryIso());
